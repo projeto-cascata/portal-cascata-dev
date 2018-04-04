6baa78bf077b8a2d932c33c9e107e32e1947597a
Felipe Hargreaves <fhargreaves00@gmail.com>
2018-04-04 15:51:09 -0300

    Initial structure for user profile.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.template import loader
 
+from .models import UserProfile
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def profile(request):
+    template = loader.get_template('user_profile.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context, request))
