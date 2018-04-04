1463f04c6f3b5f4a922976c1aee8528854fee669
Bernardo Henrique <bernardohrl.unb@gmail.com>
2018-04-04 16:01:48 -0300

    Generate Unique Enrollment Number #3
    Co-authored-by: Bernardo Henrique <bernardohrl.unb@gmail.com>
    Co-authored-by: Mariana Picolo <mariananpicolo@hotmail.com>

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class User(models.Model):
+    enrollment = models.models.IntegerField(primary_key=True, default = generate_enrollment)
+
+
+
+def generate_enrollment():
+    now = datetime.datetime.now()
+    year = str(abs(now.year) % 100)
+    
+    users = Users.objects.all()
+    if not users:
+        return (year + "0000")
+
+    users.sort(key=lambda user: user.enrollment, reverse=True)
+
+    id = int(users[0].enrollment[2:])
+    id = id+1
+
+    return (year + str(id).zfill(4))
