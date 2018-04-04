25d59514d35841f2fa0f792eb2be1224c5c61715
Leticia de Souza <leticia.souza212@gmail.com>
2018-04-04 17:48:07 -0300

    Add url route
    Co-authored-by: Bernardo Henrique <bernardohrl.unb@gmail.com>
    Co-authored-by: Leticia de Souza <leticia.souza212@gmail.com>

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import loader
 
+def login(request):
+    template = loader.get_template('login.html')
+    return render(request, 'login.html', {})
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
