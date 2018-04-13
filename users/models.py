c8cf0a558c3d90f18343701202f845ff00187c5c
Felipe Hargreaves <fhargreaves00@gmail.com>
2018-04-13 10:44:08 -0300

    Adapt custom user model to extend Django's AbstractBaseUser.
    Co-authored-by: Felipe Hargreaves <fhargreaves00@gmail.com>
    Co-authored-by: Leticia de Souza <leticia.souza212@gmail.com>

--- users/models.py
+++ users/models.py
@@ -1,11 +1,14 @@
-from django.db import models
 import datetime
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.core.validators import MinLengthValidator
 
 def generate_enrollment():
     now = datetime.datetime.now()
     year = str(abs(now.year) % 100)
 
-    users = User.objects.all()
+    users = get_user_model().objects.all()
     if not users:
         return (year + "0000")
 
@@ -16,12 +19,25 @@ def generate_enrollment():
 
     return (year + str(id).zfill(4))
 
-class User(models.Model):
-    enrollment = models.CharField(max_length=10, primary_key=True, default = generate_enrollment)
-    name = models.CharField(max_length=200, null=False, blank=False)
-    cpf = models.IntegerField(blank=False)
-    RG  = models.IntegerField(blank=False)
+class User(AbstractBaseUser):
+    enrollment = models.CharField('Matrícula', max_length=10, primary_key=True, default = generate_enrollment)
+    first_name = models.CharField(max_length=40, blank=False)
+    last_name = models.CharField(max_length=80, blank=False)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    cpf = models.CharField(max_length=11, validators=[MinLengthValidator(11)], blank=False)
+    rg = models.CharField(max_length=20, blank=False)
     date_birth = models.DateField(blank=False)
     phone_number = models.IntegerField(blank=False)
-    email = models.CharField(max_length=200, blank=False)
     picture = models.ImageField(upload_to='images/', default='images/default.svg')
+
+    USERNAME_FIELD = enrollment
+    REQUIRED_FIELDS = [first_name, email]
+
+    def get_full_name(self):
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        return self.first_name
+
