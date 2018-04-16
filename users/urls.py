ad7d79e4c1ddf16d4dc051ba69876f45938b4d8b
Clarissa Borges <clarissalimab08@gmail.com>
2018-04-15 21:36:29 -0300

    Add navbar template

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     url(r'^login/$', login, {'template_name': 'login.html'}, name='login'),
     path('profile/<int:user_id>/', views.profile),
-    url(r'^members_list/$', views.members_list)
+    url(r'^members_list/$', views.members_list),
 ]
